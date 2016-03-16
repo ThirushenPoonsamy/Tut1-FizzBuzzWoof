afa5d22610b88a0512f6000601403e03cac02027
ThirushenPoonsamy <thirushenpoonsamy@gmail.com>
2016-03-16 12:18:08 +0100

    check numbers between 1 and 20 that are divisible by 3 and 5

---
+++ Source.cpp
@@ -0,0 +1,22 @@
+# include <iostream>
+
+using namespace std;
+
+int main()
+{
+	for (int i = 1; i < 21; i++)
+	{
+		if (i % 3 == 0)
+		{
+			if (i % 5 == 0)
+			{
+				cout <<"FizzBuzz"<<endl;
+			}
+			
+
+	}
+		 
+
+ 	}
+ return 0;
+}
