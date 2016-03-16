9cd8aabb969d820198f2d709e7e46e98c043732b
ThirushenPoonsamy <thirushenpoonsamy@gmail.com>
2016-03-16 12:27:02 +0100

    final

--- Source.cpp
+++ Source.cpp
@@ -1,22 +1,74 @@
+
 # include <iostream>
 
 using namespace std;
 
+
 int main()
 {
-	for (int i = 1; i < 21; i++)
-	{
-		if (i % 3 == 0)
-		{
-			if (i % 5 == 0)
-			{
-				cout <<"FizzBuzz"<<endl;
-			}
-			
 
-	}
-		 
+for (int i = 1; i < 21; i++)
+{
 
- 	}
- return 0;
+if (i % 3 == 0)
+{
+
+if (i % 5 == 0)
+{
+
+cout <<"fizzbuzz"<<endl;
+
+}
+
+else
+
+{
+
+
+cout << "fizz" << endl;
+
+}
+
+}
+
+else if (i % 5 == 0)
+
+{
+
+cout << "buzz" << endl;
+
+}
+
+else if (i % 15 == 0)
+
+{
+
+cout << "fizzbuzzwoof" << endl;
+
+}
+
+
+else
+
+{
+
+cout << i <<endl;
+
+}
+
+
+
+
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
 }
